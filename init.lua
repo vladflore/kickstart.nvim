25098241c545a5f720eb26479493afc500d61d6b
Vlad Flore <vlad.flore@exb.de>
2024-07-05 15:29:12 +0200

    Add autoclose.nvim plugin for improved code editing experience

--- init.lua
+++ init.lua
@@ -889,6 +889,7 @@ require('lazy').setup({
       --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     end,
   },
+  {'m4xshen/autoclose.nvim', opts = {}},
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
